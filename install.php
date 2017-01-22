da31225e5214bca6a3ba159736d1fb191a47677f
Bartosz Mikolajczak <b.mikolajczak@booklet.pl>
2017-01-22 13:27:31 +0100

    Working on install script

---
+++ install.php
@@ -0,0 +1,10 @@
+<?php
+$app_directory = dirname(dirname(dirname(__FILE__)));
+
+// Setup app folders
+echo $app_directory;
+
+
+//if (!file_exists('/Users/bartek/Sites/framework/app')) {
+//    mkdir('/Users/bartek/Sites/framework/app', 0777, true);
+//}
