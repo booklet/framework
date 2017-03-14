274775c990bc0eda49051d4fccdfd4e265bea669
Booklet <mikolajczakbartosz@gmail.com>
2017-03-14 08:40:19 +0100

    Add time constants in DateTime untils class

--- src/utils/DateTimeUntils.php
+++ src/utils/DateTimeUntils.php
@@ -1,6 +1,14 @@
 <?php
 class DateTimeUntils
 {
+    const SECONDS_IN_A_MINUTE = 60;
+    const SECONDS_IN_A_HOUR = 3600;
+    const SECONDS_IN_AN_HOUR = 3600;
+    const SECONDS_IN_A_DAY = 86400;
+    const SECONDS_IN_A_WEEK = 604800;
+    const SECONDS_IN_A_MONTH = 2592000;
+    const SECONDS_IN_A_YEAR = 31536000;
+
     public static function daysInMonth($year, $month)
     {
         return cal_days_in_month(CAL_GREGORIAN, $month, $year);
