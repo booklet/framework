333f03c7d35e2e62903268f04e1a17ac9ea212fa
Booklet <b.mikolajczak@booklet.pl>
2017-09-26 10:04:08 +0200

    Add normalize files arry untils method

--- src/utils/ArrayUntils.php
+++ src/utils/ArrayUntils.php
@@ -2,14 +2,50 @@
 class ArrayUntils
 {
     /**
-    * Check if array is associative or sequential
-    * ['a', 'b', 'c'] // false
-    * ["0" => 'a', "1" => 'b', "2" => 'c'] // false
-    * ["1" => 'a', "0" => 'b', "2" => 'c'] // true
-    * ["a" => 'a', "b" => 'b', "c" => 'c'] // true
-    */
+     * Check if array is associative or sequential
+     * ['a', 'b', 'c'] // false
+     * ["0" => 'a', "1" => 'b', "2" => 'c'] // false
+     * ["1" => 'a', "0" => 'b', "2" => 'c'] // true
+     * ["a" => 'a', "b" => 'b', "c" => 'c'] // true.
+     */
     public static function isAssoc($arr)
     {
         return array_keys($arr) !== range(0, count($arr) - 1);
+    }
+
+    public static function normalizeFilesArray($files)
+    {
+        $output = [];
+        foreach ($files as $base_key => $file) {
+            if (is_array($file['name'])) {
+                $file_keys = array_keys($file['name']);
+                foreach ($file_keys as $file_key) {
+                    if (is_array($file['name'][$file_key])) {
+                        $keys = array_keys($file['name'][$file_key]);
+                        foreach ($keys as $key) {
+                            $output[$base_key][$file_key][$key] = [
+                                'name' => $file['name'][$file_key][$key],
+                                'type' => $file['type'][$file_key][$key],
+                                'tmp_name' => $file['tmp_name'][$file_key][$key],
+                                'error' => $file['error'][$file_key][$key],
+                                'size' => $file['size'][$file_key][$key],
+                            ];
+                        }
+                    } else {
+                        $output[$base_key][$file_key] = [
+                            'name' => $file['name'][$file_key],
+                            'type' => $file['type'][$file_key],
+                            'tmp_name' => $file['tmp_name'][$file_key],
+                            'error' => $file['error'][$file_key],
+                            'size' => $file['size'][$file_key],
+                        ];
+                    }
+                }
+            } else {
+                $output[$base_key] = $file;
+            }
+        }
+
+        return $output;
     }
 }
--- tests/untils/ArrayUntilsTest.php
+++ tests/untils/ArrayUntilsTest.php
@@ -6,7 +6,121 @@ class ArrayUntilsTest extends TesterCase
         $is_array = ArrayUntils::isAssoc(['item1', 'item2', 'item3']);
         Assert::expect($is_array)->to_equal(false);
 
-        $is_array = ArrayUntils::isAssoc(['attr1'=>'item1', 'attr2'=>'item2', 'attr2'=>'item3']);
+        $is_array = ArrayUntils::isAssoc(['attr1' => 'item1', 'attr2' => 'item2', 'attr2' => 'item3']);
         Assert::expect($is_array)->to_equal(true);
+    }
+
+    public function testNormalizeFilesArray()
+    {
+        // images[]
+        $files = [
+            'images' => [
+                'name' => [
+                    'test1.jpg',
+                    'test2.jpg',
+                ],
+                'type' => [
+                    'image/jpeg',
+                    'image/jpeg',
+                ],
+                'tmp_name' => [
+                    '/tmp/nsl54Gs',
+                    '/tmp/1sl54GC',
+                ],
+                'error' => [
+                    0,
+                    0,
+                ],
+                'size' => [
+                    1715,
+                    5368,
+                ],
+            ],
+        ];
+
+        $normalize_files = [
+            'images' => [
+                [
+                    'name' => 'test1.jpg',
+                    'type' => 'image/jpeg',
+                    'tmp_name' => '/tmp/nsl54Gs',
+                    'error' => 0,
+                    'size' => 1715,
+                ],
+                [
+                    'name' => 'test2.jpg',
+                    'type' => 'image/jpeg',
+                    'tmp_name' => '/tmp/1sl54GC',
+                    'error' => 0,
+                    'size' => 5368,
+                ],
+            ],
+        ];
+
+        $normalize_array = ArrayUntils::normalizeFilesArray($files);
+        Assert::expect($normalize_array)->to_equal($normalize_files);
+    }
+
+    public function testNormalizeFilesArrayNested()
+    {
+        // images[files][]
+        $files = [
+            'images' => [
+                'name' => [
+                    'files' => [
+                        'test1.jpg',
+                        'test2.jpg',
+                    ],
+                ],
+                'type' => [
+                    'files' => [
+                        'image/jpeg',
+                        'image/jpeg',
+                    ],
+                ],
+                'tmp_name' => [
+                    'files' => [
+                        '/tmp/nsl54Gs',
+                        '/tmp/1sl54GC',
+                    ],
+                ],
+                'error' => [
+                    'files' => [
+                        0,
+                        0,
+                    ],
+                ],
+                'size' => [
+                    'files' => [
+                        1715,
+                        5368,
+                    ],
+                ],
+            ],
+        ];
+
+        $normalize_files = [
+            'images' => [
+                'files' => [
+                    [
+                        'name' => 'test1.jpg',
+                        'type' => 'image/jpeg',
+                        'tmp_name' => '/tmp/nsl54Gs',
+                        'error' => 0,
+                        'size' => 1715,
+                    ],
+                    [
+                        'name' => 'test2.jpg',
+                        'type' => 'image/jpeg',
+                        'tmp_name' => '/tmp/1sl54GC',
+                        'error' => 0,
+                        'size' => 5368,
+                    ],
+                ],
+            ],
+        ];
+
+        $normalize_array = ArrayUntils::normalizeFilesArray($files);
+        Assert::expect($normalize_array)->to_equal($normalize_files);
     }
 }
