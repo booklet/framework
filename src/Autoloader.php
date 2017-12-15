08367f76bb4ee6b9ab173bcabea0681640e28079
Bartosz Mikolajczak <b.mikolajczak@booklet.pl>
2017-12-15 22:33:25 +0100

    Add support to autoload classes form app/modules directory who used namespaces

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -1,20 +1,49 @@
 <?php
 class Autoloader
 {
-    private $directory_name;
+    private $directory;
 
-    public function __construct($directory_name)
+    public function __construct($directory)
     {
-        $this->directory_name = $directory_name;
+        $this->directory = $directory;
     }
 
     public function autoload($class_name)
     {
-        $file_name = $class_name . '.php';
-        $file = $this->directory_name . '/' . $file_name;
-        if (file_exists($file) == false) {
+        $filename = $class_name;
+
+        // Support to load classes form app/modules directory who used namespaces
+        if ($this->isClassNamespaceFromModulesDriectory($class_name) and
+            $this->isClassNamespaceEqualCurrentModuleDirectory($class_name)) {
+            $filename = $this->getFileClassNameFormNamespaceClassName($class_name);
+        }
+
+        $file_path = $this->directory . '/' . $filename . '.php';
+
+        if (file_exists($file_path) == false) {
             return false;
         }
-        include $file;
+
+        include $file_path;
+    }
+
+    private function isClassNamespaceFromModulesDriectory($class_name)
+    {
+        return strpos($class_name, '\\') !== false and strpos($this->directory, 'app/modules') !== false;
+    }
+
+    private function isClassNamespaceEqualCurrentModuleDirectory($class_name)
+    {
+        list($module_name, $file_class_name) = explode('\\', $class_name);
+        $module_name = strtolower($module_name);
+
+        return strpos($this->directory, "app/modules/$module_name") !== false;
+    }
+
+    private function getFileClassNameFormNamespaceClassName($class_name)
+    {
+        $arr = explode('\\', $class_name);
+
+        return end($arr);
     }
 }
