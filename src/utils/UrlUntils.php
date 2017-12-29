d51278019addd556cc599994bb0f5c1fd3de2b12
Booklet <b.mikolajczak@booklet.pl>
2017-12-29 13:57:50 +0100

    Update Url Untils

--- src/utils/UrlUntils.php
+++ src/utils/UrlUntils.php
@@ -1,8 +1,17 @@
 <?php
 class UrlUntils
 {
-    public static function getHDomainWithProtocol($url)
+    public static function getDomainWithProtocol($url)
     {
-        return parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST);
+        if (strpos($url, 'http://') === false && strpos($url, 'https://') === false) {
+            $url = 'http://' . $url;
+        }
+
+        $parsed_url = parse_url($url);
+        if (isset($parsed_url['host'])) {
+            $url = $parsed_url['scheme'] . '://' . preg_replace('#^www\.(.+\.)#i', '$1', $parsed_url['host']);
+        }
+
+        return $url;
     }
 }
--- tests/untils/UrlUntilsTest.php
+++ tests/untils/UrlUntilsTest.php
@@ -3,8 +3,20 @@ class UrlUntilsTest extends TesterCase
 {
     public function testGetHost()
     {
-        $host = UrlUntils::getHDomainWithProtocol('https://booklet.pl/test/subpatach?param=val#test');
-
+        $host = UrlUntils::getDomainWithProtocol('https://www.booklet.pl/test/subpatch?param=val#test');
         Assert::expect($host)->to_equal('https://booklet.pl');
+
+        $host = UrlUntils::getDomainWithProtocol('http://www.booklet.pl/test/subpatch?param=val#test');
+        Assert::expect($host)->to_equal('http://booklet.pl');
+
+        $host = UrlUntils::getDomainWithProtocol('www.booklet.pl/test/subpatch?param=val#test');
+        Assert::expect($host)->to_equal('http://booklet.pl');
+
+        $host = UrlUntils::getDomainWithProtocol('booklet.pl');
+        Assert::expect($host)->to_equal('http://booklet.pl');
+
+        // TODO Add support to invalid urls
+        // $host = UrlUntils::getDomainWithProtocol('booklet');
+        // Assert::expect($host)->to_equal(false);
     }
 }
